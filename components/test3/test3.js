b2054638722f62fb63e444698bfdf33a0d247139
ifer-itcast <yangkangkang@itcast.cn>
2021-04-02 17:52:36 +0800

    behaviors

---
+++ components/test3/test3.js
@@ -0,0 +1,24 @@
+const myBehavior = require('../../behaviors/my-behavior.js')
+Component({
+  behaviors: [myBehavior],
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
