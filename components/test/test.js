65ff795bf361366b7c0da985803f9fceecb4a831
ifer-itcast <yangkangkang@itcast.cn>
2021-04-02 15:20:21 +0800

    数据监听

--- components/test/test.js
+++ components/test/test.js
@@ -1,41 +1,26 @@
 // components/test/test.js
 Component({
-  options: {
-    styleIsolation: 'shared'
+  data: {
+    n1: 0,
+    n2: 0,
+    sum: 0
   },
-  /**
-   * 组件的属性列表
-   */
-  properties: {
-    // max: Number
-    max: {
-      type: Number,
-      value: 10
+  methods: {
+    addN1() {
+      this.setData({
+        n1: this.data.n1 + 1
+      })
+    },
+    addN2() {
+      this.setData({
+        n2: this.data.n2 + 1
+      })
     }
   },
-
-  /**
-   * 组件的初始数据
-   */
-  data: {
-    count: 0
-  },
-
-  /**
-   * 组件的方法列表
-   */
-  methods: {
-    addCount() {
-      if(this.data.count >= this.properties.max) return
+  observers: {
+    'n1, n2': function(n1, n2) {
       this.setData({
-        count: this.data.count + 1
-      })
-      this._showCount()
-    },
-    _showCount() {
-      wx.showToast({
-        title: 'count 是' + this.data.count,
-        icon: 'none'
+        sum: n1 + n2
       })
     }
   }
