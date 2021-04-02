1aa0c760afa80113143f58eba57162dd3594d22b
ifer-itcast <yangkangkang@itcast.cn>
2021-04-02 13:24:46 +0800

    data和properties以及methods

--- components/test/test.js
+++ components/test/test.js
@@ -7,20 +7,36 @@ Component({
    * 组件的属性列表
    */
   properties: {
-
+    // max: Number
+    max: {
+      type: Number,
+      value: 10
+    }
   },
 
   /**
    * 组件的初始数据
    */
   data: {
-
+    count: 0
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-
+    addCount() {
+      if(this.data.count >= this.properties.max) return
+      this.setData({
+        count: this.data.count + 1
+      })
+      this._showCount()
+    },
+    _showCount() {
+      wx.showToast({
+        title: 'count 是' + this.data.count,
+        icon: 'none'
+      })
+    }
   }
 })
